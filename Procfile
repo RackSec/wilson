a531f50652bf6a7ccc118f210a0901df0b075864
Laurens Van Houtven <_@lvh.io>
2015-06-15 16:37:05 -0700

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/wilson.jar clojure.main -m wilson.server
---
+++ env/prod/cljs/wilson/prod.cljs
@@ -0,0 +1,7 @@
+(ns wilson.prod
+  (:require [wilson.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
