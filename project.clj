3ab7ac04352f8ace727a6ef1fa7be5437488cafa
Laurens Van Houtven <_@lvh.io>
2015-06-15 22:21:19 -0700

    Add description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject wilson "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :description "Opinionated Reagent bindings for Bootstrap components."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
