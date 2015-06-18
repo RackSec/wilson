3e55080e7258f8424eef6a08ab7d6f4811979911
Laurens Van Houtven <_@lvh.io>
2015-06-18 12:53:17 -0700

    Make the tests pass

---
+++ src/cljs/wilson/grid.cljs
@@ -0,0 +1,14 @@
+(ns wilson.grid
+  (:require [wilson.dom :refer [with-class]]
+            [clojure.string :refer [join]]))
+
+(defn centered
+  "Assigns a width and an offset to the elem so that it is centered
+  within its parent, assuming the parent is also a part of a grid."
+  ([window-class width elem]
+   (let [grid-cls (join "-" ["col" window-class width])
+         half (quot (- 12 width) 2)
+         offset-cls (join "-" ["col" window-class "offset" half])]
+     (with-class (join " " [grid-cls offset-cls]) elem)))
+  ([width elem]
+   (centered "md" width elem)))
