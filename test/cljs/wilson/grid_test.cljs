1db7a77e2ebbfc06307a54c967fc0904f61b6195
Laurens Van Houtven <_@lvh.io>
2015-06-18 12:51:30 -0700

    Add another test

--- test/cljs/wilson/grid_test.cljs
+++ test/cljs/wilson/grid_test.cljs
@@ -3,6 +3,9 @@
             [cljs.test :refer-macros [deftest is]]))
 
 (deftest centered-test
+  (is (= (g/centered "sm" 6 [:div])
+         [:div {:class "col-sm-6 col-sm-offset-3"}])
+      "centered elem of width 6")
   (is (= (g/centered 6 [:div])
          [:div {:class "col-md-6 col-md-offset-3"}])
       "default window class is ``md''"))
