278092e4a0242e829adc589c3f20e53d7f36c8a3
Laurens Van Houtven <_@lvh.io>
2015-06-18 12:49:57 -0700

    Run them

--- test/cljs/wilson/runner.cljs
+++ test/cljs/wilson/runner.cljs
@@ -2,8 +2,10 @@
   (:require [doo.runner :refer-macros [doo-tests]]
             [wilson.dom-test]
             [wilson.utils-test]
-            [wilson.form-test]))
+            [wilson.form-test]
+            [wilson.grid-test]))
 
 (doo-tests 'wilson.dom-test
            'wilson.utils-test
-           'wilson.form-test)
+           'wilson.form-test
+           'wilson.grid-test)
