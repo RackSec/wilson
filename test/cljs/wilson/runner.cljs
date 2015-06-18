0570bf22b7ecb0f999882c668e5548ac80a4836c
Laurens Van Houtven <_@lvh.io>
2015-06-18 01:34:30 -0700

    Run them

--- test/cljs/wilson/runner.cljs
+++ test/cljs/wilson/runner.cljs
@@ -1,7 +1,9 @@
 (ns shrieker.runner
   (:require [doo.runner :refer-macros [doo-tests]]
             [wilson.dom-test]
-            [wilson.utils-test]))
+            [wilson.utils-test]
+            [wilson.form-test]))
 
 (doo-tests 'wilson.dom-test
-           'wilson.utils-test)
+           'wilson.utils-test
+           'wilson.form-test)
