b3c8b01dbaae621dae6641ffd848d1b566b6fbb8
Laurens Van Houtven <_@lvh.io>
2015-06-24 18:35:29 -0500

    Version 0.7.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wilson "0.6.0"
+(defproject wilson "0.7.0-SNAPSHOT"
   :description "Opinionated Reagent bindings for Bootstrap components."
   :url "https://www.github.com/racksec/wilson"
   :license {:name "Eclipse Public License"
