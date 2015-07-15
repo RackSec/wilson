c89892e0c6ead8af473230acbd48ef4a41fb5aba
Laurens Van Houtven <_@lvh.io>
2015-07-15 12:30:41 -0700

    Update Clojure dependency

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 
   :source-paths ["src/clj" "src/cljs"]
 
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring-server "0.4.0"]
                  [cljsjs/react "0.13.3-0"]
                  [reagent "0.5.0"]
