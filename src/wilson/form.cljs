145dae0198103556afaa5ea845caf2a743f8e65a
Laurens Van Houtven <_@lvh.io>
2015-09-03 16:14:24 -0700

    Make the tests pass

--- src/wilson/form.cljs
+++ src/wilson/form.cljs
@@ -1,20 +1,26 @@
 (ns wilson.form
-  (:require [wilson.dom :refer [with-attrs]]))
+  (:require [wilson.dom :refer [with-attrs with-class]]))
 
 (defn ^:private form-elem
-  [id label input]
-  [:div {:class "form-group"}
-   [:label {:class "col-sm-2 control-label" :for id} label]
-   [:div {:class "col-sm-10"}
-    (let [cls (cond
-                (= (first input) :p) "form-control-static"
-                (= (get-in input [1 :type]) "file") "form-control-static"
-                :default "form-control")]
-      (with-attrs {:id id :class cls :name id} input))]])
+  ([id label input]
+   (form-elem id label input {}))
+  ([id label input {:keys [label-cls input-container-cls]}]
+   [:div {:class "form-group"}
+    (with-class label-cls
+      [:label {:class "control-label" :for id} label])
+    [:div {:class input-container-cls}
+     (let [cls (cond
+                 (= (first input) :p) "form-control-static"
+                 (= (get-in input [1 :type]) "file") "form-control-static"
+                 :default "form-control")]
+       (with-attrs {:id id :class cls :name id} input))]]))
+
 (defn ^:private horiz-form-elem
   "A horizontal form element, with the label and input horizontally
   next to each other."
   ([id label input]
    (horiz-form-elem id label input {}))
   ([id label input opts]
-   (form-elem id label input (merge {} opts))))
+   (let [opts (merge {:label-cls "col-sm-2"
+                      :input-container-cls "col-sm-10"} opts)]
+     (form-elem id label input opts))))
