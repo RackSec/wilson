a078fe3227d1e897ce35a91d075ab9396161e2bd
Laurens Van Houtven <_@lvh.io>
2016-04-13 08:39:15 -0500

    Tweak docstring

--- src/wilson/dom.cljs
+++ src/wilson/dom.cljs
@@ -13,10 +13,11 @@
 (defn prepare-keys
   [ks]
   "Prepares keys for use with wilson.dom/table.
-  `ks` should be a collection of singular keys or vectors of keys
-  pointing at nested data. Vectors are interpreted as paths to the relevant data as per `get-in`.
-  They also come with a ::descr function in metadata, which describes the
-  path using dot notation (e.g.: [:a :b :c] will become 'A.b.c'."
+
+  `ks` should be a collection of singular keys or vectors of keys pointing at
+  nested data. Vectors are interpreted as paths to the relevant data as per
+  `get-in`.  They also come with ::descr metadata, so they will be displayed
+  as a dotted path by default, e.g.: [:a :b :c] will become 'A.b.c'."
   (map (fn [k]
          (if (vector? k)
            (let [f #(get-in % k)
