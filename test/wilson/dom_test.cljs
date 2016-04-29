ea31c19ff130e71ffe758e20d8fa64f04adf94be
Laurens Van Houtven <_@lvh.io>
2016-04-29 17:06:34 -0500

    Fix the bug

--- test/wilson/dom_test.cljs
+++ test/wilson/dom_test.cljs
@@ -179,18 +179,18 @@
                 [:td "true"]]]]))))
 
 (deftest sorted-table-test
-  (let [sorted-table-rows [{:a 1 :b "B" :c -6}
-                           {:a 4 :b "A" :c 2}
-                           {:a 3 :b "D" :c 4}]
-        sorted-table-ks (d/prepare-keys [:a :b :c])
-        component (d/sorted-table sorted-table-ks sorted-table-rows)
-        [headers rows] (match component
-                         [:table _
-                          [:thead
-                           [:tr & headers]]
-                          [:tbody & rows]]
-                         [headers rows])]
-    [headers rows]))
+  (let [rows [{:a 1 :b "B" :c -6}
+               {:a 4 :b "A" :c 2}
+               {:a 3 :b "D" :c 4}]
+        ks (d/prepare-keys [:a :b :c])
+        component (d/sorted-table ks rows)
+        [table-headers table-rows] (match component
+                                     [:table _
+                                      [:thead
+                                       [:tr & table-headers]]
+                                      [:tbody & table-rows]]
+                                     [table-headers table-rows])]
+    [table-headers table-rows]))
 
 (deftest button-test
   (let [t "Some text"
